856be366a695b85aea1c1269d28486ebb9024f48
Soumick-Pyne <42906929+Soumick-Pyne@users.noreply.github.com>
2020-06-18 14:46:18 +0530

    Add files via upload

---
+++ Performance Measures/sharpe_sortino.py
@@ -0,0 +1,27 @@
+# =============================================================================
+# Measuring the performance of a buy and hold strategy - sharpe & sortino
+# =============================================================================
+
+# Import necesary libraries
+import yfinance as yf
+import numpy as np
+import datetime as dt
+
+# Download historical data for required stocks
+ticker = "^GSPC"
+SnP = yf.download(ticker,dt.date.today()-dt.timedelta(1825),dt.datetime.today())
+
+
+def sharpe(DF,rf):
+    "function to calculate sharpe ratio ; rf is the risk free rate"
+    df = DF.copy()
+    sr = (CAGR(df) - rf)/volatility(df)
+    return float(sr)
+    
+def sortino(DF,rf):
+    "function to calculate sortino ratio ; rf is the risk free rate"
+    df = DF.copy()
+    df["daily_ret"] = DF["Adj Close"].pct_change()
+    neg_vol = df[df["daily_ret"]<0]["daily_ret"].std() * np.sqrt(252)
+    sr = (CAGR(df) - rf)/neg_vol
+    return float(sr)
